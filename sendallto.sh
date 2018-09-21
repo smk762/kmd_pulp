84460a32bbcc430de227d43e18c51be5f03a02c5
smk762 <35845239+smk762@users.noreply.github.com>
2018-09-21 12:35:58 +0800

    Update sendallto.sh

--- sendallto.sh
+++ sendallto.sh
@@ -1,6 +1,6 @@
 # Sends all funds in wallet for each Staked asset chain to a common native address. Used to consolidate funds (does not migrate across chains!)
 if [[ $(komodo-cli validateaddress ${1} | jq -r '.isvalid') != false ]]; then
-	ac_json=$(curl https://raw.githubusercontent.com/blackjok3rtt/StakedNotary/master/assetchains.json 2>/dev/null)
+	ac_json=$(curl https://raw.githubusercontent.com/StakedChain/StakedNotary/master/assetchains.json 2>/dev/null)
 	for chain in $(echo "${ac_json}" | jq  -r '.[].ac_name'); do
 	  balance=$(komodo-cli -ac_name=${chain} getbalance)
 	  if [[ $balance > 0 ]]; then
