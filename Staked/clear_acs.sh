694c70762bd43ae746ef2a5d437cb0730df9161e
smk762 <smk762@iinet.net.au>
2018-09-25 19:19:59 +0800

    a couple more lazy scripts

--- Staked/clear_acs.sh
+++ Staked/clear_acs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-ac_json=$(curl https://raw.githubusercontent.com/blackjok3rtt/StakedNotary/master/assetchains.json 2>/dev/null)
+ac_json=$(curl https://raw.githubusercontent.com/StakedChain/StakedNotary/master/assetchains.json 2>/dev/null)
 for row in $(echo "${ac_json}" | jq  -r '.[].ac_name'); do
 	chain=$(echo $row)
+	echo "deleting ${chain} blocks and chainstate"
 	rm -rf /home/$USER/.komodo/${chain}/blocks
         rm -rf /home/$USER/.komodo/${chain}/chainstate
 done
+echo "done"
