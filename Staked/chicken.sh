9e61db7b337577eba7ded5de53f0d84477771000
smk762 <35845239+smk762@users.noreply.github.com>
2018-09-28 16:38:34 +0800

    Update chicken.sh

--- Staked/chicken.sh
+++ Staked/chicken.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# WARNING - SCRIPT STILL IN TESTING - USE AT OWN RISK
+# WARNING - SCRIPT STILL IN TESTING - USE AT OWN RISK
+# WARNING - SCRIPT STILL IN TESTING - USE AT OWN RISK
+# WARNING - SCRIPT STILL IN TESTING - USE AT OWN RISK
+echo "WARNING - SCRIPT STILL IN TESTING - USE AT OWN RISK"
+echo "WARNING - SCRIPT STILL IN TESTING - USE AT OWN RISK"
+echo "WARNING - SCRIPT STILL IN TESTING - USE AT OWN RISK"
+echo "WARNING - SCRIPT STILL IN TESTING - USE AT OWN RISK"
+
+prompt_confirm() {
+  while true; do
+    read -r -n 1 -p "${1:-Continue?} [y/n]: " REPLY
+    case $REPLY in
+      [yY]) echo ; return 0 ;;
+      [nN]) echo ; return 1 ;;
+      *) printf " \033[31m %s \n\033[0m" "invalid input"
+    esac
+  done
+}
+prompt_confirm "Continue? (exit and ask in Discord if unsure)" || exit 0
+
 # This script creates transactions to quickly migrate your balance evenly across all active chains on the same -ac_cc id
 total_balance=0
 num_migrates=0
