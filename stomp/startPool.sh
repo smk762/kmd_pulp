328a516c6ec27793c58fe834a5037fd7c2ae20a2
smk762 <smk762@iinet.net.au>
2018-09-30 05:13:12 +0000

    startPool force pubkey use

--- stomp/startPool.sh
+++ stomp/startPool.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 /home/$USER/Knomp/install/redis-stable/src/redis-server /home/$USER/Knomp/install/redis-stable/redis.conf &
-cd /home/$USER/kmd_pulp/stomp
+cd /home/$USER/kmd_pulp/Staked/lazy
 ./startStaked.sh &
-komodod
-sleep 60
+sleep 120
 cd /home/$USER/Knomp
 nohup npm start &
 #tail -f nohup.out
---
+++ stomp/stopPool.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pkill -9 redis
+pkill -9 node
+komodo-cli stop
+staked-cli stop
+
