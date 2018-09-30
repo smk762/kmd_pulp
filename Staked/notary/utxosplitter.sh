c550feec5617022fb06f229d1a9c08ce63f45434
smk762 <smk762@iinet.net.au>
2018-09-30 09:53:04 +0000

    utxosplit with assetchains.json

---
+++ Staked/notary/utxosplitter.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+cd "${BASH_SOURCE%/*}" || exit
+
+# Optionally just split UTXOs for a single coin
+# e.g "KMD"
+specific_coin=$1
+
+
+kmd_target_utxo_count=100
+kmd_split_threshold=50
+
+other_target_utxo_count=25
+other_split_threshold=15
+
+date=$(date +%Y-%m-%d:%H:%M:%S)
+
+calc() {
+  awk "BEGIN { print "$*" }"
+}
+
+if [[ -z "${specific_coin}" ]]; then
+  echo "----------------------------------------"
+  echo "Splitting UTXOs - ${date}"
+  echo "KMD target UTXO count: ${kmd_target_utxo_count}"
+  echo "KMD split threshold: ${kmd_split_threshold}"
+  echo "Other target UTXO count: ${other_target_utxo_count}"
+  echo "Other split threshold: ${other_split_threshold}"
+  echo "----------------------------------------"
+fi
+
+ac_json=$(curl https://raw.githubusercontent.com/StakedChain/StakedNotary/master/assetchains.json 2>/dev/null)
+num_chains=$(echo "${ac_json}" | jq  -r '. | length');
+for chain_params in $(echo "${ac_json}" | jq  -c -r '.[]'); do
+    ac_name=$(echo $chain_params | jq -r '.ac_name')
+  if [[ -z "${specific_coin}" ]] || [[ "${specific_coin}" = "${ac_name}" ]]; then
+    cli=$(komodo-cli -ac_name=${ac_name})
+
+    if [[ "${ac_name}" = "KMD" ]]; then
+      cli=$(komodo-cli)
+      target_utxo_count=$kmd_target_utxo_count
+      split_threshold=$kmd_split_threshold
+    else
+      target_utxo_count=$other_target_utxo_count
+      split_threshold=$other_split_threshold
+    fi
+
+    satoshis=10000
+    amount=$(calc $satoshis/100000000)
+    listunspent=$(${cli} listunspent)
+    if [[ "${listunspent}" = "" ]]; then
+      echo "[$coin] Listuspent call failed aborting!"
+    else
+      utxo_count=$(echo ${listunspent} | jq -r '.[].amount' | grep ${amount} | wc -l)
+      echo "[${ac_name}] Current UTXO count is ${utxo_count}"
+
+      utxo_required=$(calc ${target_utxo_count}-${utxo_count})
+
+      if [[ ${utxo_required} -gt ${split_threshold} ]]; then
+        echo "[${ac_name}] Splitting ${utxo_required} extra UTXOs"
+        json=$(./splitfunds.sh ${ac_name} ${utxo_required})
+        txid=$(echo ${json} | jq -r '.txid')
+        if [[ ${txid} != "null" ]]; then
+          echo "[${ac_name}] Split TXID: ${txid}"
+        else
+          echo "[${ac_name}] Error: $(echo ${json} | jq -r '.error')"
+        fi
+      fi
+    fi
+  fi
+done
